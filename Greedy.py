1ffedd836f86e7580e02f65efdd3fef7aecb16a7
grosjean1 <elise.grosjean2@gmail.com>
2024-01-01 18:13:36 +0100

    greedy maj

--- Greedy.py
+++ Greedy.py
@@ -8,83 +8,126 @@ from BasicTools.FE import FETools as FT
 import numpy as np
 from scipy import linalg
 
-def orthogonality_check(Matrix,CorrelationMatrix): #to check if the RB is L2 orthogonal
+def orthoGS(basis_vectors,cptmodes,m,snapshotCorrelationOperator):
+    residuals2=np.array(np.reshape(basis_vectors,((cptmodes,m))))
+    basis_vectorsVec=np.reshape(basis_vectors,((cptmodes,m)))
+            
+    residuals2[-1,:]=basis_vectorsVec[-1]-sum((residuals2[k,:]*np.dot(snapshotCorrelationOperator.dot(residuals2[k]),basis_vectorsVec[-1]) for k in range(residuals2.shape[0]-1)))            
+        
+    basis_vectors[-1]=np.reshape(residuals2[-1,:],((m,1)))
+    return basis_vectors
+
+          
+def orthogonality_check(Matrix,CorrelationMatrix,orthoN=False):
     """
     This function check for the pairwise orthogonality of the new basis
     """
     list_ = list(Matrix)
-    dot_matrix = np.array([[np.dot(CorrelationMatrix.dot(item1), item2) for item1 in list_] for item2 in list_])
-    if (dot_matrix - np.eye(dot_matrix.shape[0]) < 1e-10).all():
+    dot_matrix = np.array([[np.dot(CorrelationMatrix.dot(item1), item2.T) for item1 in list_] for item2 in list_])
+    A=dot_matrix
+
+    if(orthoN==False):
+        A=np.diag(np.diag(dot_matrix))
+    else:
+        A= np.eye(dot_matrix.shape[0])
+
+    if (dot_matrix - A < 1e-12).all():
+        print(True)
         return True
     else:
-        error = dot_matrix - np.eye(dot_matrix.shape[0])
+        error = dot_matrix - A
+     
         print("max error with identity: ",np.max(np.abs(error)))
         return False
 
-#TolGreedy is for the time compression
-def Greedy(snapshots,NumerOfTimesSteps,snapshotCorrelationOperator,NumberOfModes,h1ScalarProducMatrix=None, TolGreedy=1e-6):
+
+def greedy_algorithm(snapshots,NumerOfTimesSteps,snapshotCorrelationOperator, NumberOfModes):
     """
     Compute the greedy reduced basis using a greedy algorithm.
     """
    
+    TolGreedy=1e-5 #Tolerance on the time steps
     DegreesOfFreedom=np.shape(snapshotCorrelationOperator)[0]
     NbParam=len(snapshots)
-    ListeParam=[k for k in range(NbParam)]
     
     # initialization: First parameter
-    
     ind=0 #first parameter (orthonormalized, so norm(snapshot) > 1e-10)
-    ListeIndex=[ind] 
+
     basis_vectors=[] # basis functions list
     snapshotsList=snapshots.copy()
-    snapshotsArray=snapshotsList.pop(ind) 
+    snapshotsArray=snapshotsList.pop(ind) #snapshots with the parameter ind...
     
-    residuals=np.array(snapshotsArray) #all time steps for first parameter 
+    residuals=np.array(snapshotsArray) #...all time steps for first parameter 
     n,m = residuals.shape
-    
+            
     tol=1
-    cptmodes=1
+    cptmodes=1 #First mode
     cpt=0
-    matVecProduct0=np.zeros((m,n))
-    NormTestMax0=np.zeros(m)
-    #for k in range(1):
+    
+   
+    tolold=tol
+    matVecProduct0=snapshotCorrelationOperator.dot(residuals.T)
+
+    # FIRST PARAMETER SELECTION //LOOP OVER TIME
+    TimeIndices=[]
+    #while(tol >= TolGreedy and NumberOfModes+1> cptmodes):
     GlobalIndex=[]
-    while(tol >= TolGreedy and NumberOfModes+1> cptmodes):
+
+    while(2 >=  cptmodes and NumberOfModes+1> cptmodes):
+    
+        
         matVecProduct= snapshotCorrelationOperator.dot(residuals.T)
         NormTestMax=np.sqrt(np.diag(np.dot(residuals,matVecProduct)))
-        
-        if cpt==0:
-            matVecProduct0=matVecProduct 
-            NormTestMax0=NormTestMax
-            TimeIndex=np.argmax(NormTestMax0)
-            basis_vectors.append((np.reshape(residuals[TimeIndex,:],((m,1)))/NormTestMax[TimeIndex])) #first time index=random
-            print(cptmodes," / ",NumberOfModes)
-            print(NormTestMax[ind],"/",TolGreedy ,"(treshold)")
-            ListeTimeIndices=[TimeIndex]
-        else:
-            TimeIndex= np.argmax(NormTestMax)
-            tol=np.max(NormTestMax)
-            print(cptmodes," / ",NumberOfModes, "(same parameter, new time step)")
-            print(tol,"/",TolGreedy ,"(treshold)")
+        TimeIndex=np.argmax(NormTestMax)
+          
+        mask = (TimeIndex not in TimeIndices)
+        if mask==False: #check if not already in the time steps 
+                
+            TimeIndexOld=TimeIndex
+            filtered_list = [elt for indx,elt in enumerate(list(np.arange(len(NormTestMax)))) if elt not in TimeIndices]
+            TimeIndex = filtered_list[np.argmax(NormTestMax[filtered_list])] 
+            print("New Time Index: ", TimeIndex)
 
-            basis_vectors.append(np.reshape(residuals[TimeIndex,:]/tol,((m,1))))
-            ListeTimeIndices.append(TimeIndex)
-        print("list",ListeTimeIndices)
-        residuals -= np.outer(basis_vectors[-1],np.dot(basis_vectors[-1].T,matVecProduct0)).T
+        TimeIndices.append(TimeIndex)
+        
+        tol=NormTestMax[TimeIndex]
+
+        basis_vectors.append(np.reshape(residuals[TimeIndex,:],((m,1)))/tol) # time index, added in the modes
+        
+        print(cptmodes," / ",NumberOfModes, "(same parameter, new time step)")
+        print(NormTestMax[TimeIndex],"/",TolGreedy ,"(threshold)")
+  
+       
+        residuals -= np.outer(basis_vectors[-1],np.dot(basis_vectors[-1].T,matVecProduct0)).T #update
+        # Gram-Schmidt
+        orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+        orthocpt=0
+        
+        while(10>=orthocpt and orthogonality==False):
+            basis_vectors=orthoGS(basis_vectors,cptmodes,m,snapshotCorrelationOperator)
+    
+            orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+            orthocpt+=1
+
         cptmodes+=1
         cpt+=1
-        
-    GlobalIndex.append([ind,ListeTimeIndices])
-    ListeParam.pop(ind)
-    print(GlobalIndex)
-    print("nb basis", len(basis_vectors))
-    # greedy on the parameters
+        """
+        if(tol > tolold):
+            print(tol)
+            break;
+        """
+        tolold=tol
+        print(cptmodes)
+      
+    GlobalIndex.append([ind,TimeIndices])    
+    # PARAMETER SELECTION
+    
 
-    matrix=np.array(snapshotsList)
+  
     cptParam=1
     
-    while(NumberOfModes+1>cptmodes):
-       
+    while(NumberOfModes>cptmodes):
+        matrix=np.array(snapshotsList)    
         residuals = np.reshape(matrix,(((NbParam-cptParam)*n,m)))
         
         matVecProduct0 = snapshotCorrelationOperator.dot(residuals.T)
@@ -94,22 +137,39 @@ def Greedy(snapshots,NumerOfTimesSteps,snapshotCorrelationOperator,NumberOfModes
         matVecProduct = snapshotCorrelationOperator.dot(residuals.T)
         NormTestMax = np.sqrt(np.diag(np.dot(residuals,matVecProduct)))
         Index = np.argmax(NormTestMax)
+        
         IndexParam = int(Index/n+0.00001)
-        print("glob new index for new param: ", Index, "IndexParam: ", IndexParam, "TF : ", n)
+        print("New param: ",IndexParam)
         tol =np.max(NormTestMax)
     
-        basis_vectors.append(np.reshape(residuals[Index,:]/tol,((m,1))))
-        print(cptmodes," / ",NumberOfModes, "(new parameter)")
-        print(tol,"/",TolGreedy,"(treshold)")
-        cptmodes+=1
+        basis_vectors.append(np.reshape(residuals[Index,:]/tol,((m,1)))) #Update
+        
+        print(cptmodes," / ",NumberOfModes, "(new parameter chosen)")
+        print(tol,"/",TolGreedy,"(threshold)")
+        
+        if (cptParam!=1):
+            cptmodes+=1
+
+        #Gram-Schmidt
+        orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+        orthocpt=0
+        while(10>=orthocpt and orthogonality==False):
+            basis_vectors=orthoGS(basis_vectors,cptmodes,m,snapshotCorrelationOperator)
+          
+            orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+            orthocpt+=1
+
+            
         snapshotsArray=snapshotsList.pop(IndexParam)
-        matrix=np.array(snapshotsList)
+    
         residuals=np.array(snapshotsArray)
         cpt=0
-        tol=1
-        ListeTimeIndices=[int(Index/(NbParam-cptParam)+0.00001)]
-
-        while(tol >= TolGreedy and NumberOfModes >= cptmodes): #greedy on time steps
+        #tol=1
+        oldtol=tol
+        TimeIndices=[]
+        # TIME STEPS SELECTION
+        TimeIndices.append(int(Index/m+0.00001))
+        while(4>=cpt and NumberOfModes > cptmodes): #greedy on time steps
          
             if cpt==0:
                 matVecProduct0=snapshotCorrelationOperator.dot(residuals.T)
@@ -119,47 +179,80 @@ def Greedy(snapshots,NumerOfTimesSteps,snapshotCorrelationOperator,NumberOfModes
             matVecProduct = snapshotCorrelationOperator.dot(residuals.T)
             NormTestMax = np.sqrt(np.diag(np.dot(residuals,matVecProduct)))
             TimeIndex = np.argmax(NormTestMax)
-            ListeTimeIndices.append(TimeIndex)
+            mask = (TimeIndex not in TimeIndices)
+            if mask==False: #check if not already in the time steps 
+                
+                TimeIndexOld=TimeIndex
+                filtered_list = [elt for indx,elt in enumerate(list(np.arange(len(NormTestMax)))) if elt not in TimeIndices]
+                TimeIndex = filtered_list[np.argmax(NormTestMax[filtered_list])]
+                print("New Time Index: ", TimeIndex)
+                #break
+                
+            print("time: ",TimeIndex)
+            TimeIndices.append(TimeIndex)
             tol = np.max(NormTestMax)
+            #if(tol >= oldtol-1e-8):
+            #    break;
             basis_vectors.append(np.reshape(residuals[TimeIndex,:]/tol,((m,1))))
-            residuals -= np.outer(basis_vectors[-1],np.dot(basis_vectors[-1].T,matVecProduct0)).T
-            print(cptmodes," / ",NumberOfModes, "(same parameter, new time step)")
-            print(tol,"/",TolGreedy,"(treshold)")
             cptmodes+=1
+            print(cptmodes," / ",NumberOfModes, "(same parameter, new time step)")
+            print(tol,"/",TolGreedy,"(threshold)")
+            
+            #Gram-Schmidt
+            orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+            orthocpt=0
+            
+            while(10>=orthocpt and orthogonality==False):
+                basis_vectors=orthoGS(basis_vectors,cptmodes,m,snapshotCorrelationOperator)
+       
+                orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+                orthocpt+=1
+            
+            residuals -= np.outer(basis_vectors[-1],np.dot(basis_vectors[-1].T,matVecProduct0)).T
             cpt+=1
+            """
+            if(tol >= tolold-1e-8):
+                #cptParam+=1
+                print(tol)
+                break;
+            """
+            tolold=tol
             
-            
-            
-        print(ListeTimeIndices, " ", IndexParam+cptParam)
-        print(GlobalIndex)
-        ind=ListeParam.pop(IndexParam)
-        GlobalIndex.append([ind,ListeTimeIndices])
+            #if(tol >= oldtol-1e-8):
+            #    break;
         cptParam+=1
-    print(len(basis_vectors))
-    basis_vectors=np.reshape(basis_vectors,((NumberOfModes,m)))
+        GlobalIndex.append([IndexParam,TimeIndices])  
+
+    orthocpt=0
+    orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+    while(10>=orthocpt and orthogonality==False):
+        basis_vectors=orthoGS(basis_vectors,cptmodes,m,snapshotCorrelationOperator)
+        
+        orthogonality=orthogonality_check(np.reshape(basis_vectors,((cptmodes,m))),snapshotCorrelationOperator)
+        orthocpt+=1
+
+
     reducedOrderBasisU=np.array(np.reshape(basis_vectors,((NumberOfModes,m))))
     
-    orthogonality=orthogonality_check(reducedOrderBasisU,snapshotCorrelationOperator)
     cpt=0
-    while (orthogonality==False and 10>=cpt):       #Gram-schmidt 
-        for i in range(1,NumberOfModes):
-              
-            reducedOrderBasisU[i,:]=basis_vectors[i]-sum((reducedOrderBasisU[k,:]*np.dot(snapshotCorrelationOperator.dot(reducedOrderBasisU[k]),basis_vectors[i]) for k in range(i)))            
-            reducedOrderBasisU[i]/=np.sqrt(np.dot((snapshotCorrelationOperator.dot(reducedOrderBasisU[i])),reducedOrderBasisU[i]))
-        for j in range(1,NumberOfModes):
-            basis_vectors[j]=reducedOrderBasisU[j,:]
-        cpt+=1
-        orthogonality=orthogonality_check(reducedOrderBasisU,snapshotCorrelationOperator)
-        print(orthogonality)
+    for i in range(NumberOfModes):
+        reducedOrderBasisNorm=np.sqrt(reducedOrderBasisU[i,:]@(snapshotCorrelationOperator@reducedOrderBasisU[i,:]))
+        reducedOrderBasisU[i,:]/=reducedOrderBasisNorm #np.sqrt(M[i,i]) #L2 orthonormalization
 
-       ### H1 Orthogonalization
-    if(h1ScalarProducMatrix!=None):
-        K=np.zeros((NumberOfModes,NumberOfModes)) #rigidity matrix
-        M=np.zeros((NumberOfModes,NumberOfModes)) #mass matrix
-        for i in range(NumberOfModes):
+
+    
+    ### H1 Orthogonalization
+    
+    #print(" H1 orthogonalization... ")
+    RB=[]
+    for k in range(NumberOfModes,NumberOfModes+1): #for k in range(1,NumberOfModes+1):
+        """
+        K=np.zeros((k,k)) #rigidity matrix
+        M=np.zeros((k,k)) #mass matrix
+        for i in range(k):
             matVecH1=h1ScalarProducMatrix.dot(reducedOrderBasisU[i,:])
             matVecL2=snapshotCorrelationOperator.dot(reducedOrderBasisU[i,:])
-            for j in range(NumberOfModes):
+            for j in range(k):
                 if i>=j:
             
                     K[i,j]=np.dot(matVecH1,reducedOrderBasisU[j,:])
@@ -168,20 +261,37 @@ def Greedy(snapshots,NumerOfTimesSteps,snapshotCorrelationOperator,NumberOfModes
                     M[j,i]=M[i,j]
     
     
-        # on resoud Kv=lambd Mv
+        # Solving Kv=lambd Mv
         eigenValues,vr=linalg.eig(K, b=M) #eigenvalues + right eigenvectors
+        print(eigenValues)
         idx = eigenValues.argsort()[::-1]
-        eigenValues = eigenValues[idx]
-        eigenVectors = vr[:, idx]
-        reducedOrderBasisU=np.dot(eigenVectors.transpose(),reducedOrderBasisU)
-
-        for i in range(NumberOfModes):
-            reducedOrderBasisNorm=np.sqrt(reducedOrderBasisU[i,:]@(snapshotCorrelationOperator@reducedOrderBasisU[i,:]))
-            reducedOrderBasisU[i,:]/=reducedOrderBasisNorm#np.sqrt(M[i,i]) #L2 orthonormalization
+        eigenValues = eigenValues#[idx]
     
-        orthogonality=orthogonality_check(reducedOrderBasisU,snapshotCorrelationOperator)
-        print(orthogonality)
-    return reducedOrderBasisU,NumberOfModes,GlobalIndex
+        print("EV : ",eigenValues)
+        print("sqrt(EV) : ",np.sqrt(eigenValues))
+        print(np.shape(reducedOrderBasisU))
+        
+        reducedOrderBasisUp=reducedOrderBasisU[0:k,:]#[idx,:]
+        
+        eigenVectors = vr#[idx,:]
+        
+        reducedOrderBasisUp=np.dot(eigenVectors.transpose(),reducedOrderBasisUp)
+        print(np.shape(eigenVectors))
+    
+    """
+        reducedOrderBasisUp=reducedOrderBasisU
+        orthogonality=orthogonality_check(reducedOrderBasisUp,snapshotCorrelationOperator,False)
+        print("ortho l2?", orthogonality)
+        #orthogonality=orthogonality_check(reducedOrderBasisUp,h1ScalarProducMatrix,False)
+        #print("ortho h1?",orthogonality)
+        reducedOrderBasisUt=reducedOrderBasisUp
+    
+        #for i in range(k):
+        #    reducedOrderBasisUt[i]/=np.sqrt(eigenValues[i].real)
+        RB.append(reducedOrderBasisUt)
+    return RB,NumberOfModes,GlobalIndex
+
+
 
 
 
